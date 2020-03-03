2d92d57e4c288eca1354a27298ebf681b86f2557
JunSeob Kim <46683933+BenzeneKim@users.noreply.github.com>
2020-03-04 02:03:41 +0900

    modified files

--- MainServer.py
+++ MainServer.py
@@ -2,20 +2,28 @@ from flask import Flask as flask
 from flask import render_template
 app = flask(__name__)
 
-image_link = str
+image_link_1 = str
+image_link_2 = str
 
 
 @app.route('/')
 def index():
-    checkError = str(input("change the color (y/n)"))
-    if (checkError == "y"):
-        image_link = "images/boxB.png"
-
+    checkError = str(input("change the color"))
+    if (checkError == "11"):
+        image_link_1 = "images/boxB.png"
+        image_link_2 = "images/boxB.png"
+    elif (checkError == "10"):
+        image_link_1 = "images/boxB.png"
+        image_link_2 = "images/boxR.png"
+    elif (checkError == "01"):
+        image_link_1 = "images/boxR.png"
+        image_link_2 = "images/boxB.png"
     else:
         image_link = "images/boxR.png"
-
-    print(image_link)
-    return render_template('index.html', image_file = image_link )
+        image_link = "images/boxR.png"
+    print(image_link_1)
+    print(image_link_2)
+    return render_template('index.html', image_file_1 = image_link_1, image_file_2 = image_link_2 )
 
 @app.route('/info')
 def info():
